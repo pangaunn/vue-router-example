585ff0ed0999cff9d1db3fa97870e86f8935a583
aunn <aunn_it08@hotmail.com>
2018-04-28 04:18:07 +0700

    delete HelloWorld component and add GettingStarted component

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,5 +1,22 @@
 <template>
-  <div>
-    Nav
-  </div>
+  <ul>
+    <li><router-link to="/getting-started">Getting Started</router-link></li>
+    <li><a href="#">Dynamic Route</a></li>
+    <li><a href="#">Nested Routes</a></li>
+    <li><a href="#">Programmatic Navigation</a></li>
+  </ul>
 </template>
+
+<style>
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import GettingStarted from '@/pages/GettingStarted'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/getting-started',
+      name: 'GettingStarted',
+      component: GettingStarted
     }
   ]
 })
